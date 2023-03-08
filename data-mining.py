8e60f798a0c26f14f6f15a47a13f963b082106ec
Yvonne511 <yw4142@nyu.edu>
2023-03-08 15:44:52 -0500

    Data Mining

---
+++ data-mining.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import datetime
+import requests
+import json
+import praw
+import os
+from dotenv import load_dotenv
+load_dotenv()
+# Set up the Reddit API
+
+# my_client_id = os.getenv('my_client_id')
+# my_client_secret = os.getenv('my_client_secret')
+# my_user_agent = os.getenv('my_user_agent')
+# reddit = praw.Reddit(client_id=my_client_id, client_secret=my_client_secret, user_agent=my_user_agent)
+
+# posts = []
+# ml_subreddit = reddit.subreddit('MachineLearning')
+# for post in ml_subreddit.hot(limit=10):
+#     posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+# posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+# print(posts)
+
+class data_mining:
+
+    def epoch_datatime(self, dt):
+        epoch_start = datetime.datetime(1970, 1, 1)
+        delta = dt - epoch_start.date()
+        epoch_time = delta.days * 86400
+        return epoch_time
+
+    def epoch(self, year, month, day):
+        dt = datetime.datetime(year, month, day)
+        epoch_start = datetime.datetime(1970, 1, 1)
+        return int((dt - epoch_start).total_seconds())
+    
+    def getPushshiftData_Comment(self, query, after, before, sub, size = 500):
+        url = 'https://api.pushshift.io/reddit/search/comment?q='+str(query)+'&subreddit='+str(sub)+'&after='+str(after)+'&before='+str(before)+'&size='+str(size)
+        print(url)
+        r = requests.get(url)
+        data = json.loads(r.text)
+        return data['data']
+    
+    def getPushshiftData_Submission(self, query, after, before, sub, size = 500):
+        url = 'https://api.pushshift.io/reddit/search/submission?q='+str(query)+'&subreddit='+str(sub)+'&after='+str(after)+'&before='+str(before)+'&size='+str(size)
+        print(url)
+        r = requests.get(url)
+        data = json.loads(r.text)
+        return data['data']
+    
+start_date = datetime.date(2021, 1, 11)
+end_date = datetime.date(2021, 2, 5)
+delta = datetime.timedelta(days=1)
+current_date = start_date
+while current_date <= end_date:
+    start_epoch=data_mining().epoch_datatime(current_date)
+    end_epoch=data_mining().epoch_datatime(current_date+delta)
+    data = data_mining().getPushshiftData_Comment('gamestop', start_epoch, end_epoch, 'wallstreetbets', 500)
+    print(data)
+    if len(data) == 0:
+            continue
+    else:
+        df = pd.DataFrame(data)
+        df.to_csv('/Users/yw511/VSCode/Machine Learning/reddit-investment-sentiment/data/wsb_'+str(current_date)+'.csv', index=False)
+    current_date += delta
