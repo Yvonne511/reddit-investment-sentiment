031fba68a00496302a9c17c8328fd7314dd7ec27
Huixuan Wang <whx987172@gmail.com>
2023-03-22 13:18:51 -0400

    update praw and using try for url requests

--- data-mining.py
+++ data-mining.py
@@ -2,10 +2,11 @@ import pandas as pd
 import datetime
 import requests
 import json
-import praw
-import os
-from dotenv import load_dotenv
-load_dotenv()
+import time
+# import praw
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
 # Set up the Reddit API
 
 # my_client_id = os.getenv('my_client_id')
@@ -34,10 +35,17 @@ class data_mining:
         return int((dt - epoch_start).total_seconds())
     
     def getPushshiftData_Comment(self, query, after, before, sub, size = 500):
-        url = 'https://api.pushshift.io/reddit/search/comment?q='+str(query)+'&subreddit='+str(sub)+'&after='+str(after)+'&before='+str(before)+'&size='+str(size)
-        print(url)
-        r = requests.get(url)
-        data = json.loads(r.text)
+        data = None
+        while not data:
+            try:
+                url = 'https://api.pushshift.io/reddit/search/comment?q='+str(query)+'&subreddit='+str(sub)+'&after='+str(after)+'&before='+str(before)+'&size='+str(size)
+                # print(url)
+                r = requests.get(url)
+                data = json.loads(r.text)
+            except:
+                time.sleep(5)
+        if len(data['data'])==500:
+            print(str(after)+ ": "+str(len(data['data'])))
         return data['data']
     
     def getPushshiftData_Submission(self, query, after, before, sub, size = 500):
@@ -49,7 +57,7 @@ class data_mining:
 
 time_period = 60
 data_count = 0
-start_date = datetime.date(2021, 1, 11)
+start_date = datetime.date(2021, 1, 25)
 end_date = datetime.date(2021, 2, 5)
 delta = datetime.timedelta(days=1)
 current_date = start_date
@@ -61,12 +69,16 @@ while current_date <= end_date:
     while current_epoch_by_hour<=end_epoch:
         temp_data = data_mining().getPushshiftData_Comment('gamestop', current_epoch_by_hour, current_epoch_by_hour+60, 'wallstreetbets', 500)
         current_epoch_by_hour += 60
-        data.extend(temp_data)
+
+        for d in temp_data:
+            new_d = {"id":d["id"], "utc_datetime_str":d["utc_datetime_str"], "body":d["body"]}
+            data.append(new_d)
+
     if len(data) == 0:
             continue
     else:
         df = pd.DataFrame(data)
         data_count += df.shape[0]
         print('Data Count: ', data_count)
-        df.to_csv('/Users/yw511/VSCode/Machine Learning/reddit-investment-sentiment/data/wsb_'+str(current_date)+'.csv', index=False)
+        df.to_csv('./gamestop/wsb_'+str(current_date)+'.csv', index=False)
     current_date += delta
