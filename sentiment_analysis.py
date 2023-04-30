4e45cc30cc2ef5f7458d6b6be4b68d5d091e8a52
Yvonne511 <yw4142@nyu.edu>
2023-04-29 20:10:05 -0400

    sentiment analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -8,77 +8,80 @@ import matplotlib.pyplot as plt
 
 class sentiment_analysis:
 
+    df_comments = pd.DataFrame(columns = ['Sentiment_Score', 'Updated_Sentiment_Score', 'Date'])
+    df_submissions = pd.DataFrame(columns = ['Sentiment_Score', 'Updated_Sentiment_Score', 'Date'])
+    analyzer = None
+    updataed_analyzer = None
+
     def __init__(self):
         self.analyzer = SentimentIntensityAnalyzer()
+        self.updataed_analyzer = SentimentIntensityAnalyzer()
+        # Update VADER lexicon with new words from file vader_lexicon.csv
+        new_words = {}
+        with open('./vader_lexicon.csv', 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    # each file contains Word, Score, Word, Score, Word, Score, Word, Score
+                    pairs = line.split(' ')
+                    for i in range(0, len(pairs), 2):
+                        new_words[pairs[i]] = float(pairs[i+1])
+        self.updataed_analyzer.lexicon.update(new_words)
 
     def get_sentiment(self, text):
         return self.analyzer.polarity_scores(text)["compound"]
-
-df_comments = pd.DataFrame(columns = ['Sentiment_Score', 'Date'])
-
-def getSubmissionSentimentScore():
-    ## Part 1: Generate Sentiment Score for each submission
-    cwd = os.getcwd()
-    target_dir = os.path.join(cwd, 'data', 'gamestop', 'submissions')
-    file_path_array = []
-    df_submissions = pd.DataFrame()
-    # Get all the files in dir
-    print(target_dir)
-    for filepath in pathlib.Path(target_dir).glob('**/*'):
-        if filepath.is_file():
-            file_path_array.append(filepath.relative_to(cwd).as_posix())
-    for file_path in file_path_array:
-        file_path = os.path.join(cwd, file_path)
-        df = pd.read_csv(file_path)
-        df['Sentiment_Score'] = df['body'].apply(lambda x: sentiment_analysis().get_sentiment(x))
-        df["Date"] = df["utc_datetime_str"].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-        df.drop(columns=['utc_datetime_str', 'body'], inplace=True)
-        df_submissions = pd.concat([df_submissions, df], ignore_index=True)
-    print(df_submissions.shape)
-    df_submissions.to_csv('./wsb_sentiment.csv', index=False)
-
-def getCommentSentimentScore():
-    ## Part 1: Generate Sentiment Score for each submission
-    total_count = 0
-    cwd = os.getcwd()
-    target_dir = os.path.join(cwd, 'data', 'gamestop', 'comments')
-    file_path_array = []
-    # Get all the files in dir
-    # print(target_dir)
-    for filepath in pathlib.Path(target_dir).glob('**/*'):
-        if filepath.is_file():
-            file_path_array.append(filepath.relative_to(cwd).as_posix())
-    for file_path in file_path_array:
-        file_path = os.path.join(cwd, file_path)
-        with open(filepath, 'r') as f:
-            first_line = f.readline().strip()
-            count_str = first_line.split(': ')[-1]
-            count = int(count_str)
-            total_count += count
-
-            # Read rest of file as JSON Array
-            json_list = (''.join(f.readlines())).strip().split('\n')
-            for json_obj in json_list:
-                data = json.loads(json_obj)
-                comments = data['comments']
-                for comment in comments:
-                    df_comments.loc[len(df_comments)] = [sentiment_analysis().get_sentiment(comment['text']), comment['time']]
-                    tranverseComments(comment)
-
-def tranverseComments(commentObj):
-    if len(commentObj['comments']) > 0:
-        for comment in commentObj['comments']:
-            df_comments.loc[len(df_comments)] = [sentiment_analysis().get_sentiment(comment['text']), comment['time']]
-            tranverseComments(comment)
-    else:
-        return
     
-def getLayeredCommentSentimentScore():
-    getCommentSentimentScore()
-    print(df_comments.shape)
-    df_comments["Date"] = df_comments["Date"].apply(lambda x: datetime.datetime.fromtimestamp(x))
-    df_comments.sort_values(by=['Date'], inplace=True)
-    df_comments.to_csv('./wsb_comment_sentiment.csv', index=False)
+    def get_updated_sentiment(self, text):
+        return self.updataed_analyzer.polarity_scores(text)["compound"]
+
+    def getSubmissionSentimentScore(self):
+        ## Part 1: Generate Sentiment Score for each submission
+        cwd = os.getcwd()
+        target_dir = os.path.join(cwd, 'data', 'gem&gamestop', 'submissions')
+        file_path_array = []
+        df_submissions = pd.DataFrame()
+        # Get all the files in dir
+        print(target_dir)
+        for filepath in pathlib.Path(target_dir).glob('**/*'):
+            if filepath.is_file() and 'chunk' in filepath and 'csv' in filepath:
+                file_path_array.append(filepath.relative_to(cwd).as_posix())
+        for file_path in file_path_array:
+            file_path = os.path.join(cwd, file_path)
+            df = pd.read_csv(file_path)
+            df['Sentiment_Score'] = df['body'].apply(lambda x: self.get_sentiment(x))
+            df['Updated_Sentiment_Score'] = df['body'].apply(lambda x: self.get_updated_sentiment(x))
+            df["Date"] = df["utc_datetime_str"].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+            df.drop(columns=['utc_datetime_str', 'body'], inplace=True)
+            df_submissions = pd.concat([df_submissions, df], ignore_index=True)
+
+    def getCommentSentimentScore(self):
+        ## Part 1: Generate Sentiment Score for each submission
+        cwd = os.getcwd()
+        target_dir = os.path.join(cwd, 'data', 'gme&gamestop', 'comments')
+        file_path_array = []
+        # Get all the files in dir
+        # print(target_dir)
+        for filepath in pathlib.Path(target_dir).glob('**/*'):
+            if filepath.is_file():
+                file_path_array.append(filepath.relative_to(cwd).as_posix())
+        for file_path in file_path_array:
+            file_path = os.path.join(cwd, file_path)
+            with open(filepath, 'r') as f:
+                json_list = (''.join(f.readlines())).strip().split('\n')
+                for json_obj in json_list:
+                    data = json.loads(json_obj)
+                    comments = data['comments']
+                    for comment in comments:
+                        self.df_comments.loc[len(self.df_comments)] = [self.get_sentiment(comment['text']), self.get_updated_sentiment(comment['text']), comment['time']]
+                        self.tranverseComments(comment)
+
+    def tranverseComments(self, commentObj):
+        if len(commentObj['comments']) > 0:
+            for comment in commentObj['comments']:
+                self.df_comments.loc[len(self.df_comments)] = [self.get_sentiment(comment['text']), self.get_updated_sentiment(comment['text']), comment['time']]
+                self.tranverseComments(comment)
+        else:
+            return
 
 # Read JSON file and convert to dataframe
 df_gme = pd.DataFrame()
@@ -94,23 +97,28 @@ df_gme.sort_values(by=['Date'], inplace=True)
 df_gme.set_index('Date', inplace=True)
 print(df_gme.head())
 
-df = pd.read_csv('./wsb_sentiment_resampled.csv')
-df_comments = pd.read_csv('./wsb_comment_sentiment.csv')
-df["Date"] = df["Date"].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-df["Sentiment_Score"] = df["Sentiment_Score"].apply(lambda x: float(x)*200)
-df_comments["Date"] = df_comments["Date"].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-df = pd.concat([df, df_comments], ignore_index=True)
-df = df[df['Date'] >= '2021-01-25']
-df = df[df['Date'] <= '2021-02-05']
-df.sort_values(by=['Date'], inplace=True)
-df.set_index('Date', inplace=True)
-df = df.resample('T').sum(numeric_only=True)
+sa = sentiment_analysis()
+sa.getCommentSentimentScore()
+df_comments = sa.df_comments
+print(df_comments.head())
 
-# Left join the two dataframes on Date
-df = df.join(df_gme, how='left')
+# df = pd.read_csv('./wsb_sentiment_resampled.csv')
+# df_comments = pd.read_csv('./wsb_comment_sentiment.csv')
+# df["Date"] = df["Date"].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+# df["Sentiment_Score"] = df["Sentiment_Score"].apply(lambda x: float(x)*200)
+# df_comments["Date"] = df_comments["Date"].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+# df = pd.concat([df, df_comments], ignore_index=True)
+# df = df[df['Date'] >= '2021-01-25']
+# df = df[df['Date'] <= '2021-02-05']
+# df.sort_values(by=['Date'], inplace=True)
+# df.set_index('Date', inplace=True)
+# df = df.resample('T').sum(numeric_only=True)
 
-print(df.shape)
-print(df.head(10))
+# # Left join the two dataframes on Date
+# df = df.join(df_gme, how='left')
 
-df.plot()
-plt.show()
+# print(df.shape)
+# print(df.head(10))
+
+# df.plot()
+# plt.show()
